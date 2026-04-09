012151ae18256b58c6a2d60ed887869fb385a632
Nicolas Chabloz <43348620+Chabloz@users.noreply.github.com>
2026-04-09 14:40:11 +0200

    Update useJsonStorage.js

--- composables/useJsonStorage.js
+++ composables/useJsonStorage.js
@@ -1,13 +1,26 @@
 import { ref, watch } from "vue";
-import * as jsonStorage from "@/utils/jsonStorage.js";
+
+function setItem(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getItem(key, defaultValue = null) {
+  if (localStorage.getItem(key) === null) setItem(key, defaultValue);
+
+  let data;
+  try {
+    data = JSON.parse(localStorage.getItem(key));
+  } catch (e) {
+    console.warn("Value in localStorage is not valid JSON, falling back to default value.");
+    data = defaultValue;
+  }
+  return data;
+}
 
 export function useJsonStorage(key, defaultValue) {
-
   const data = ref(defaultValue);
-  data.value = jsonStorage.getItem(key, defaultValue);
-  watch(data, () => {
-    jsonStorage.setItem(key, data.value);
-  }, {deep: true});
+  data.value = getItem(key, defaultValue);
+  watch(data, () => setItem(key, data.value), {deep: true});
 
   return { data };
 }
